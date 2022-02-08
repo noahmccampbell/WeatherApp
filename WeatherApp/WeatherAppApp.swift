dfcccb3133166549982f30988a03e0f2e230aec4
Noah McCampbell <noahmccamp@aol.com>
2022-02-08 10:48:40 -0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Noah Riley McCampbell (Student) on 2/8/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
